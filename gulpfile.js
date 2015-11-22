046965f764cb88fa5787c69b73d220fbe1ce8163
Dan Gallagher <dangallagher@Dans-MacBook-Pro.local>
2015-11-21 20:21:01 -0500

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+//Requires
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    sass        = require('gulp-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    cp          = require('child_process');
+
+//Message to print for BrowserSync
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+//Function for building the Jekyll site
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+//Rebuild function for calling a reload after rebuilding jekyll 
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+//Launch BrowserSync after jekyll build
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+//Compile Sass to both _site and css folders
+gulp.task('sass', function () {
+    return gulp.src('_sass/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+//Watch function for live refresh
+gulp.task('watch', function () {
+    gulp.watch('_sass/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
